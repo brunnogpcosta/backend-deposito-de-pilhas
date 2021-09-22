39575e7ab8cc51ab2e8662e88c55b234933c6282
BrunnoCostaUNI <brunno.costa@unigranrio.br>
2021-09-22 13:33:19 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
+
+app.use(bodyParser.json())
 
 const porta = 3000
 
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.status(200)
+  res.send({ mensagem: 'Bem vindo a API do Deposito de Pilhas' });
 });
 
 app.listen(porta, () => {
   console.log("Servidor iniciado!")
 
 })
+
+module.exports = app
